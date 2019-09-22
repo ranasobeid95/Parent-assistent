37e64a18e4bd1c37abcdf13d0533cce187577120
Mohammed <mhmmade@gmail.com>
2019-09-23 02:03:30 +0300

    fix console.log

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './Components/App/index';
 import * as serviceWorker from './serviceWorker';
-console.log(App);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
