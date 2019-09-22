df5d7efc5cc316b4c165376a0eb7ad0b48be8fa9
asmaa <asmaa@pop-os.localdomain>
2019-09-22 14:06:17 +0300

    create button component relates # 33

---
+++ client/src/Components/CommonComponent/Button/Button.js
@@ -0,0 +1,15 @@
+import React from "react"; 
+import "./Button.css";
+
+export default function Button ({
+    value = "login",
+    className = "button-input",
+}){  
+    return (
+        <div className="button-div">        
+            <input type="submit" name="submit" className={className} value={value} />
+        </div>
+    );
+}
+
+   
