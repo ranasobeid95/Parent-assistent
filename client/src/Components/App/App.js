822eb348d6f325253f7cee8642c6438f7d214046
AlaaTaima <alaa.ktaima@gmail.com>
2019-09-22 13:55:27 +0300

    remove footer component from App.js file Relates #32

--- client/src/Components/App/App.js
+++ client/src/Components/App/App.js
@@ -1,10 +1,9 @@
 import React from "react";
-import { Footer } from "../CommonComponent/Footer/Footer";
 import "./App.css";
 
 function App() {
   return (
-    <div className='App'>
+    <div className="App">
     </div>
   );
 }
