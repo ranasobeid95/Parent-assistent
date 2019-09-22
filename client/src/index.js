14d5791389f42b82a420752aa4c455409c346d25
Mohammed <mhmmade@gmail.com>
2019-09-22 23:09:07 +0300

    fix wrong import

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './Components/App/App';
+import App from './Components/App/index';
 import * as serviceWorker from './serviceWorker';
+console.log(App);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
