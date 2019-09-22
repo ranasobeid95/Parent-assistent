6151dd6bb85fdb6883e3f4c2d4972672a4b00111
Mohammed <mhmmade@gmail.com>
2019-09-22 20:23:25 +0300

    convert Header to calss component

--- client/src/Components/CommonComponent/Header/Header.js
+++ client/src/Components/CommonComponent/Header/Header.js
@@ -1,23 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../../../assets/logo.png';
 import('./Header.css');
 
-const Header = () => {
-  return (
-    <header className='App-header'>
-      <div className='logo'>
-        <img src={logo} alt='Site Logo' className='site-logo' />
-      </div>
+class Header extends Component {
+  render() {
+    return (
+      <header className='App-header'>
+        <div className='logo'>
+          <img src={logo} alt='Site Logo' className='site-logo' />
+        </div>
 
-      <div className='App-identifier'>Parent assistant</div>
-      <Link className='hamburger' to='/'>
-        <div></div>
-        <div></div>
-        <div></div>
-      </Link>
-    </header>
-  );
-};
+        <div className='App-identifier'>Parent assistant</div>
+        <Link className='hamburger' to='/'>
+          <div></div>
+          <div></div>
+          <div></div>
+        </Link>
+      </header>
+    );
+  }
+}
 
 export default Header;
