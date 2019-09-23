be8bdae1f7dffcf73dac6a4666b0ca3cf90e97fd
AlaaTaima <alaa.ktaima@gmail.com>
2019-09-23 21:47:53 +0300

    change components name Relates #62

---
+++ client/src/Components/CommonComponent/TeacherTable/data.js
@@ -0,0 +1,9 @@
+// eslint-disable-next-line import/no-mutable-exports
+const data = [
+  { id: 1, teacherName: 'Abdallah Taima', subject: 'Math' },
+  { id: 2, teacherName: 'Ahmed Ali', subject: 'Arabic' },
+  { id: 3, teacherName: 'Mohammed Ali', subject: 'English' },
+  { id: 4, teacherName: 'Alaa Taima', subject: 'Science' },
+];
+
+export default data;
