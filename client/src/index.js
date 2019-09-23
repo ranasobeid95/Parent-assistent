dc7a3d64d605bd718b68cdacc9f4d999bb12f685
aminalakhsham <aminking140@gmail.com>
2019-09-23 11:43:29 +0300

    fix elint

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './Components/App/index';
-import * as serviceWorker from './serviceWorker';
+import App from './Components/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-serviceWorker.unregister();
