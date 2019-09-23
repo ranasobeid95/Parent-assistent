90b9e54749cc0c41da21f7efc23561be20edf2fd
asmaa <asmathabet2009@hotmail.com>
2019-09-23 14:16:32 +0300

    add container , header and footer to the App

--- client/src/Components/App/index.js
+++ client/src/Components/App/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import Header from '../CommonComponent/Header';
+import Footer from '../CommonComponent/Footer';
 import './index.css';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div id="root">
+      <Header />
+      <main className="container"></main>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
