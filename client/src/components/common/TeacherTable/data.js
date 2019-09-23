9d4a69f11b53f23230f8994ccbaaa1180748a05b
asmaa <asmathabet2009@hotmail.com>
2019-09-23 23:48:24 +0300

    fix conflict relates #68

---
+++ client/src/components/common/TeacherTable/data.js
@@ -0,0 +1,8 @@
+const data = [
+  { id: 1, teacherName: 'Abdallah Taima', subject: 'Math' },
+  { id: 2, teacherName: 'Ahmed Ali', subject: 'Arabic' },
+  { id: 3, teacherName: 'Mohammed Ali', subject: 'English' },
+  { id: 4, teacherName: 'Alaa Taima', subject: 'Science' },
+];
+
+export default data;
