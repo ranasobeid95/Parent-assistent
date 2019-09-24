f64249c973abab2e3883bcc9e9df57ed32a22040
asem1789 <as.1789@hotmail.com>
2019-09-24 14:48:44 +0300

    edit SignUp page with its style and some edit in App.css Relates #13

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import Header from '../common/Header';
 import Footer from '../common/Footer';
 import './index.css';
+import Signup from '../pages/SignUp';
 
 function App() {
   return (
     <>
       <Header />
-      <main className="container"></main>
+      <main className="container">
+        <Signup />
+      </main>
       <Footer />
     </>
   );
--- client/src/components/pages/SignUp/index.js
+++ client/src/components/pages/SignUp/index.js
@@ -3,17 +3,14 @@ import Button from '../../common/Button';
 import './index.css';
 
 const Signup = () => {
-  const data = ['As a Teacher', 'As a Parent'];
   return (
     <div className="wrapper-singup">
       <div className="signup">
-        {data.map((el, index) =>
-          index === 0 ? (
-            <Button value={el} className="button-input button-input__teacher" />
-          ) : (
-            <Button value={el} className="button-input" />
-          )
-        )}
+        <Button
+          value="As a Teacher"
+          className="button-input button-input__teacher"
+        />
+        <Button value="As a Parent" className="button-input" />
       </div>
     </div>
   );
