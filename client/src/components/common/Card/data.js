14ad2147df16931f9408264979f8d7b1db3c448c
AlaaTaima <alaa.ktaima@gmail.com>
2019-09-24 15:06:16 +0300

    create page for activities and change card component to class and also set style for them Relates #23

---
+++ client/src/components/common/Card/data.js
@@ -0,0 +1,28 @@
+const data = [
+  {
+    id: 1,
+    title: 'Activity-1-',
+    description:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+  },
+  {
+    id: 2,
+    title: 'Activity-2-',
+    description:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+  },
+  {
+    id: 3,
+    title: 'Activity-3-',
+    description:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+  },
+  {
+    id: 4,
+    title: 'Activity-4-',
+    description:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+  },
+];
+
+export default data;
--- client/src/components/common/Card/index.js
+++ client/src/components/common/Card/index.js
@@ -1,17 +1,33 @@
 import React from 'react';
+// import data from './data';
 import './index.css';
 
-const Card = ({ title, desc }) => {
-  return (
-    <div className="card-box">
-      <div className="card-box__title">
-        <h3 className="title">{title}</h3>
-      </div>
-      <div className="card-box__desc">
-        <p>{desc}</p>
-      </div>
-    </div>
-  );
-};
+export default class Card extends React.Component {
+  state = { data: [] };
 
-export default Card;
+  componentDidMount() {
+    // we will make a request to fetch data by using (Axios)
+  }
+
+  render() {
+    const { data } = this.state;
+    return (
+      <>
+        {!data ? (
+          <h1>loading...</h1>
+        ) : (
+          data.map(({ title, description, id }) => (
+            <div className="card-box" key={id}>
+              <div className="card-box__title">
+                <h5 className="title">{title}</h5>
+              </div>
+              <div className="card-box__desc">
+                <p>{description}</p>
+              </div>
+            </div>
+          ))
+        )}
+      </>
+    );
+  }
+}
