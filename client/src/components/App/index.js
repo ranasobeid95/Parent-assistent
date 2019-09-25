2d097b6eab867e27abd45e8a72712324c6d7adf6
Mohammed <mhmmade@gmail.com>
2019-09-25 11:32:17 +0300

    remove login component form App

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import Header from '../common/Header';
 import Footer from '../common/Footer';
-import LogIn from '../pages/LogIn';
 import './index.css';
 
 function App() {
   return (
     <>
       <Header />
-      <main className="container">
-        <LogIn />
-      </main>
+      <main className="container"></main>
       <Footer />
     </>
   );
