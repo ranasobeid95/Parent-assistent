c9f8cd6c3026cf8b44bef6da334b935ae68aac2d
asem1789 <as.1789@hotmail.com>
2019-09-25 15:40:28 +0300

    Error page Relates #25

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import Header from '../common/Header';
 import Footer from '../common/Footer';
-import Error from '../pages/Error';
 import './index.css';
 
 function App() {
   return (
     <>
       <Header />
-      <main className="container">
-        <Error />
-      </main>
+      <main className="container"></main>
       <Footer />
     </>
   );
--- client/src/components/pages/Error/index.js
+++ client/src/components/pages/Error/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './index.css';
 
-const Error = () => {
+const Error = ({
+  typeError = '500',
+  errorDesc = 'Sory the website is unavailable',
+}) => {
   return (
     <div className="error">
       <h2 className="error__title">Error</h2>
-      <h3 className="error__type">Error</h3>
-      <p className="error__desc">Sory the website is unavailable</p>
+      <h3 className="error__type">{typeError}</h3>
+      <p className="error__desc">{errorDesc}</p>
     </div>
   );
 };
