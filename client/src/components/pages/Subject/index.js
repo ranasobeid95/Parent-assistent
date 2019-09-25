e90871f2496b495c808d31b7a254c5d4a9dcea0c
Mohammed <mhmmade@gmail.com>
2019-09-25 22:51:33 +0300

    follow BEM convention relates #22

--- client/src/components/pages/Subject/index.js
+++ client/src/components/pages/Subject/index.js
@@ -5,16 +5,14 @@ import('./index.css');
 
 const Subject = ({ subject = 'Arabic' }) => {
   return (
-    <React.Fragment>
-      <div className="Student__div">
-        <p className="subject__p">{subject}</p>
-        <p className="subject__des">
-          you will find a complete description of the class
-        </p>
-        <Button className="subject__btn" value="Class Activities"></Button>
-        <Button className="subject__btn" value="Class Homeworks"></Button>
-      </div>
-    </React.Fragment>
+    <div className="subject">
+      <p className="subject__p--subject">{subject}</p>
+      <p className="subject__p--des">
+        you will find a complete description of the class
+      </p>
+      <Button className="subject__btn" value="Class Activities"></Button>
+      <Button className="subject__btn" value="Class Homeworks"></Button>
+    </div>
   );
 };
 
