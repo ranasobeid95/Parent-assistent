508f1b4e2c8a36843e7b7e4e0a5bef79b5b6424c
asem1789 <as.1789@hotmail.com>
2019-09-25 15:09:19 +0300

    write  error component Relates #25

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import Header from '../common/Header';
 import Footer from '../common/Footer';
+import Error from '../pages/Error';
 import './index.css';
 
 function App() {
   return (
     <>
       <Header />
-      <main className="container"></main>
+      <main className="container">
+        <Error />
+      </main>
       <Footer />
     </>
   );
---
+++ client/src/components/pages/Error/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './index.css';
+
+const Error = () => {
+  return (
+    <div className="error">
+      <h2 className="error__title">Error</h2>
+      <h3 className="error__type">Error</h3>
+      <p className="error__desc">Sory the website is unavailable</p>
+    </div>
+  );
+};
+
+export default Error;
