fdd9f8c3182db9dd31e4d24b8b170aa200ec7b9b
asmaa <asmathabet2009@hotmail.com>
2019-09-26 11:28:14 +0300

    change arrays elements to camel case relates #24

--- client/src/components/pages/HomeWork/data.js
+++ client/src/components/pages/HomeWork/data.js
@@ -1,13 +1,13 @@
 const data = [
   {
     id: 1,
-    Homework_description:
+    HomeworkDescription:
       'Lorem Ipsum is simply dummy text of the printing and typesetting industry Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took',
     HmURLS: [
-      'https://www.npmjs.com/package/prop-types',
-      'https://www.npmjs.com/package/prop-types',
-      'https://www.npmjs.com/package/prop-types',
-      'https://www.npmjs.com/package/prop-types',
+      { id: 1, link: 'https://www.npmjs.com/package/prop-types' },
+      { id: 2, link: 'https://www.npmjs.com/package/prop-types' },
+      { id: 3, link: 'https://www.npmjs.com/package/prop-types' },
+      { id: 4, link: 'https://www.npmjs.com/package/prop-types' },
     ],
   },
 ];
