b8d17c2f05c35dbb043f8f73acadd845fca92652
Mohammed <mhmmade@gmail.com>
2019-09-26 11:52:15 +0300

    final style changes

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import Header from '../common/Header';
 import Footer from '../common/Footer';
-import Subject from '../pages/Subject';
 import './index.css';
 
 function App() {
   return (
     <>
       <Header />
-      <main className="container">
-        <Subject />
-      </main>
+      <main className="container"></main>
       <Footer />
     </>
   );
