bfe98cdda9265eb8fafdbb6f51e8537130700cec
Mohammed <mhmmade@gmail.com>
2019-09-26 11:23:34 +0300

    fix common style issue

--- client/src/components/pages/Subject/index.js
+++ client/src/components/pages/Subject/index.js
@@ -1,17 +1,33 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Button from '../../common/Button';
 
 import('./index.css');
 
-const Subject = ({ subject = 'Arabic' }) => {
+const Subject = ({ subject = 'Arabic', CalssId, homeworkId, activityId }) => {
   return (
     <div className="subject">
-      <p className="subject__p--subject">{subject}</p>
-      <p className="subject__p--des">
-        you will find a complete description of the class
-      </p>
-      <Button className="subject__btn" value="Class Activities"></Button>
-      <Button className="subject__btn" value="Class Homeworks"></Button>
+      <div className="subject__title-box">
+        <p className="subject__p--subject">{subject}</p>
+        <p className="subject__p--des">
+          you will find a complete description of the class
+        </p>
+      </div>
+      <div className="subject__link">
+        <Link
+          className="subject__link--activities"
+          to={`${subject}/${activityId}/${CalssId}`}
+        >
+          <Button className="subject__btn" value="Class Activities"></Button>
+        </Link>
+
+        <Link
+          className="subject__link--homework"
+          to={`${subject}/${homeworkId}/${CalssId}`}
+        >
+          <Button className="subject__btn" value="Class Homeworks"></Button>
+        </Link>
+      </div>
     </div>
   );
 };
