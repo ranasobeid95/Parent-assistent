52e55dc0ad836fb7c7e232e4526c8d3ee76a07a5
asmaa <asmathabet2009@hotmail.com>
2019-09-26 22:13:19 +0300

    create app.js file relates #26

---
+++ server/app.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const express = require('express');
+const cookieParser = require('cookie-parser')
+
+const router = require('./controllers/index');
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(cookieParser());
+app.set('port', PORT);
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+app.use(router);
+
+
+module.exports = app;
