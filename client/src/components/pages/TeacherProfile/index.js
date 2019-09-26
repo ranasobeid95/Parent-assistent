26a0c572a2b7afcc75a9d719447bd98d43ee95f9
asem1789 <as.1789@hotmail.com>
2019-09-26 10:21:46 +0300

    delete renderButton and teacherTape files and put its content in parent Component, and put name of tape as constent. All of these according JAN comment Relates #19

--- client/src/components/pages/TeacherProfile/index.js
+++ client/src/components/pages/TeacherProfile/index.js
@@ -2,14 +2,18 @@ import React, { Component } from 'react';
 import './index.css';
 import TeacherImage from '../../../assets/teacher.jpg';
 import fakeData from './fakeData';
-import teacherTape from '../utils/teacherTap';
-import renderButton from '../utils/renderButton';
 
 class TeacherProfile extends Component {
   state = {
     data: {},
     activeContent: `about`,
   };
+
+  ActiveContentEnum = Object.freeze({
+    ABOUT: 'about',
+    CONTACT: 'contact',
+    INTERESTS: 'interests'
+  });
 
   componentDidMount() {
     this.setState({ data: fakeData.teacher1 });
@@ -20,8 +24,65 @@ class TeacherProfile extends Component {
     this.setState({ activeContent: tapName });
   };
 
-  render() {
+  renderButton = (contentId, text) => {
     const { activeContent } = this.state;
+    return (
+      <button
+        type="button"
+        onClick={() => this.showContent(contentId)}
+        className={`teach-profile__tap ${
+          activeContent === contentId ? 'btn-border' : ''
+        }`}
+      >
+        {text}
+      </button>
+    );
+  };
+
+  renderTape = data => {
+    const { activeContent } = this.state;
+    if (activeContent === 'about') {
+      return (
+        <div>
+          <p className="teach-profile__desc">{data.about}</p>
+        </div>
+      );
+    }
+    if (activeContent === 'contact') {
+      return (
+        <div>
+          <ul className="teach-profile__contact-list ">
+            <li>
+              <span className="contact-list__span">Full Name:&nbsp;</span>
+              {data.contact.fullName}
+            </li>
+            <li>
+              <span className="contact-list__span">mobile Number:&nbsp;</span>
+              {data.contact.mobileNumber}
+            </li>
+            <li>
+              <span className="contact-list__span">Email:&nbsp;</span>
+              {data.contact.email}
+            </li>
+          </ul>
+        </div>
+      );
+    }
+    if (activeContent === 'interests') {
+      return (
+        <div>
+          <ul className="teach-profile__interests-list">
+            {data.interests.map(el => {
+              return <li key={el.id}>{el.text}</li>;
+            })}
+          </ul>
+        </div>
+      );
+    }
+    return null;
+  };
+
+  render() {
     const { data } = this.state;
 
     return (
@@ -32,11 +93,12 @@ class TeacherProfile extends Component {
         <h3 className="teacher-name">{data.name}</h3>
         <p className="teacher-subject">{data.subject} Teacher</p>
         <div className="teach-profile__list">
-          {renderButton(activeContent, 'About', this.showContent)}
-          {renderButton(activeContent, 'Contact', this.showContent)}
-          {renderButton(activeContent, 'Interests', this.showContent)}
+          {this.renderButton(this.ActiveContentEnum.ABOUT, 'About')}
+          {this.renderButton(this.ActiveContentEnum.CONTACT, 'Contact')}
+          {this.renderButton(this.ActiveContentEnum.INTERESTS, 'Interests')}
         </div>
-        {teacherTape(activeContent, data)}
+
+        {this.renderTape(data)}
       </div>
     );
   }
