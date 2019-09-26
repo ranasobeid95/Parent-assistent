4b16b09286a33a84164cfca6bb534be04c680c54
asmaa <asmathabet2009@hotmail.com>
2019-09-26 22:30:24 +0300

    add router to app file relates #26

--- server/app.js
+++ server/app.js
@@ -6,11 +6,13 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(cookieParser());
+const router = require('./controllers/index');
+
 app.set('port', PORT);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use(express.static(path.join(__dirname, '..', 'public')));
-
+app.use(router);
 
 module.exports = app;
