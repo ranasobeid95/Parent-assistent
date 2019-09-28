91bb06b156c5b4ff52b882552f6efd82a39da30d
AlaaTaima <alaa.ktaima@gmail.com>
2019-09-28 08:29:57 +0300

    handle login-route Relates #28

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 
 const router = express.Router();
+const { login } = require('./routes/login');
+
+router.post('/parent-profiel', login);
 
 module.exports = router;
