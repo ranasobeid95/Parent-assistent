3dad0ed647c067a681f8b9d218e36c0982a140bd
Mohammed <mhmmade@gmail.com>
2019-09-28 23:56:29 +0300

    Add the route to router function

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const { signupParent } = require('./routes/signupParent');
 
 const router = express.Router();
+router.post('/api/v1/signup/parent', signupParent);
 
 module.exports = router;
