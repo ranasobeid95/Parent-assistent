77885aef302d25168617fb02f3f832b49f0a94ca
asmaa <asmathabet2009@hotmail.com>
2019-09-29 22:24:24 +0300

    cookie.test file relates #95

--- test/routes/cookie.test.js
+++ test/routes/cookie.test.js
@@ -1,1 +1,1 @@
-exports.access = '222555';
+exports.access = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwYXJlbnRpZCI6MSwibmFtZSI6InBhcmVudC1hc3Npc3RlbnQiLCJpYXQiOjE1Njk3ODQ4NzZ9.6-4TPZfgzXfCthqq_Ca_Ov3n7mzRoLDiCQdLzZMMOFQ';
