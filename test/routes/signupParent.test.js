931dda4a1dda618ca3c5c55a48f968781e5a6f1d
Mohammed <mhmmade@gmail.com>
2019-09-30 01:19:34 +0300

    Do the test for signup for Parent

---
+++ test/routes/signupParent.test.js
@@ -0,0 +1,39 @@
+const tape = require('tape');
+const supertest = require('supertest');
+const { dbBuild } = require('../../server/database/config/build');
+
+const app = require('../../server/app');
+
+dbBuild().then(
+  tape('testing the signupParent insert quary', (t) => {
+    supertest(app)
+      .post('/api/v1/signup/parent')
+      .send({
+        email: 'mhmmade@gmail.com',
+        username: 'Mohammed',
+        parentId: '12345679',
+        password: '123456789',
+      })
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end((err, res) => {
+        if (err) {
+          t.error(err);
+          t.end();
+        } else {
+          t.deepEqual(
+            res.body,
+            {
+              email: 'mhmmade@gmail.com',
+              password:
+                '$2b$10$8iVjfMD/o9YBVauixyUvyuuSasNxVzgK958dTS8VdITbOAkLghZjK',
+              user_name: 'Mohammed',
+              parent_id: 12345679,
+            },
+            'Logout test passed',
+          );
+          t.end();
+        }
+      });
+  }),
+);
