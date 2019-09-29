006abe1ee110d0a13b94919c8f440e65588b4a15
asmaa <asmathabet2009@hotmail.com>
2019-09-29 23:44:46 +0300

    fix problems to pass the test relates #95

--- server/controllers/middlewares/auth.js
+++ server/controllers/middlewares/auth.js
@@ -1,7 +1,9 @@
-const { jwt } = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
+
+const secret = process.env.SECRET_KEY;
 
 const auth = (req, res, next) => {
-  jwt.verify(req.cookies.access, process.env.secret, (err, token) => {
+  jwt.verify(req.cookies.access, secret, (err, token) => {
     if (err) {
       res.status(401).send({ statusCode: 401, error: 'Un Authorized' });
     } else {
