4ed30fb90d8dbc933f2a0ccbc32843b59d7f92ed
asmaa <asmathabet2009@hotmail.com>
2019-09-29 22:26:54 +0300

    remove index word relates #95

--- server/router/index.js
+++ server/router/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { logout } = require('../controllers');
-const { auth } = require('../controllers/middlewares/index');
+const { auth } = require('../controllers/middlewares');
 
 const router = express.Router();
 
