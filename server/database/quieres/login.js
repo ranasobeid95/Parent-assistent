5ce95c594eaba52ed215fb3f5a3bd3d94290527f
AlaaTaima <alaa.ktaima@gmail.com>
2019-09-30 01:32:29 +0300

    edit login-query test and edit login-query Relates #28

--- server/database/quieres/login.js
+++ server/database/quieres/login.js
@@ -1,7 +1,7 @@
 const connection = require('../config/connection');
 
 const loginData = (email) => connection.query(
-  'select email, password, parent.parent_id from user inner join parent on parent.user_id=user.id where user.email=$1',
+  'select email, password, parent.parent_id from "user" inner join parent on parent.user_id="user".id where "user".email=$1',
   [email],
 );
 
--- test/quieres/login.js
+++ test/quieres/login.js
@@ -5,16 +5,15 @@ const loginData = require('../../server/database/quieres/login');
 tape('testing for login qyuery', (t) => {
   const email = 'Ola200@gmail.com';
   dbBuild()
-    .then(dbFakeData)
-    .then(() => {
-      loginData(email).then((res) => {
-        t.deepEqual(
-          res.rows[0].email,
-          'Ola200@gmail.com',
-          'should be the same',
-        );
-        t.end();
-      });
+    .then(() => dbFakeData())
+    .then(() => loginData(email))
+    .then((res) => {
+      t.deepEqual(res.rows[0].email, 'Ola200@gmail.com', 'should be the same');
+      t.end();
+    })
+    .catch((err) => {
+      t.error(err);
+      t.end();
     });
 });
 
