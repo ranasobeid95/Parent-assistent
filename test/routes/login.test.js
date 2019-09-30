795a3844249a38c2737f11ab30885b6b75775f1a
AlaaTaima <alaa.ktaima@gmail.com>
2019-09-30 11:41:33 +0300

    edit login-route test Relates #28

--- test/routes/login.test.js
+++ test/routes/login.test.js
@@ -6,13 +6,13 @@ const { dbFakeData, dbBuild } = require('../../server/database/config/build');
 test('Test /login route', (t) => {
   const userInfo = {
     email: 'Ola200@gmail.com',
-    password: '123456',
+    password: '12345678',
   };
   dbBuild()
     .then(() => dbFakeData())
     .then(() => {
       supertest(app)
-        .post('/login')
+        .post('/api/v1/login')
         .send(userInfo)
         .expect(200)
         .expect('Content-Type', 'application/json; charset=utf-8')
