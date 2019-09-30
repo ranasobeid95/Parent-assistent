ec8d4fd72bed46ad607dd0c19915c27b6d97793c
asmaa <asmathabet2009@hotmail.com>
2019-09-30 13:37:18 +0300

    remove home from app.js file

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import Header from '../common/Header';
 import Footer from '../common/Footer';
-import Home from '../pages/Home';
 import './index.css';
 
 function App() {
   return (
     <>
       <Header />
-      <main className="container">
-        <Home />
-      </main>
+      <main className="container"></main>
       <Footer />
     </>
   );
