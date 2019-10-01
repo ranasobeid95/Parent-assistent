7486c8488307e95ccdc13090bd1d135129992f3e
AlaaTaima <alaa.ktaima@gmail.com>
2019-10-01 21:45:25 +0300

    remove routes from middleware file Relates #38

--- server/controllers/middlewares/index.js
+++ server/controllers/middlewares/index.js
@@ -1,12 +1,5 @@
 const { auth } = require('./auth');
 
-const { parentPRofile } = require('../routes/parentProfile');
-const { classActivities } = require('../routes/activities');
-const { homeworks } = require('../routes/homeworks');
-
 module.exports = {
   auth,
-  parentPRofile,
-  classActivities,
-  homeworks,
 };
