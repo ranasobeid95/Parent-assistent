6bf47850de855b7110256ece47bb4dd6ee58d35d
asmaa <asmathabet2009@hotmail.com>
2019-10-01 14:53:50 +0300

    add some edits relates #42

--- test/quieres/getHomeworks.test.js
+++ test/quieres/getHomeworks.test.js
@@ -6,10 +6,10 @@ tape('test for getHomeworks query', (t) => {
   const newDate = '10/1/2019';
   dbBuild()
     .then(dbFakeData)
-    .then(() => getClassHomeworks(1, 1, newDate))
+    .then(getClassHomeworks(1, 1, newDate))
     .then((res) => {
-      const expected = { homework_description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', urls: { resource1: 'https://www.github.com', resource2: 'https://www.github.com' } };
       const actual = res.rows[0];
+      const expected = { homework_description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', urls: { resource1: 'https://www.github.com', resource2: 'https://www.github.com' } };
       t.deepEqual(actual, expected, 'should be the same');
       t.end();
     })
