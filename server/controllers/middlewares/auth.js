580e84666e57d9769be1a181275398e9364b6b1b
AlaaTaima <alaa.ktaima@gmail.com>
2019-10-02 13:35:10 +0300

    solve eslint problems Relates #103

--- server/controllers/middlewares/auth.js
+++ server/controllers/middlewares/auth.js
@@ -3,7 +3,6 @@ const jwt = require('jsonwebtoken');
 const secret = process.env.SECRET_KEY;
 
 const auth = (req, res, next) => {
-  console.log(req.cookies);
   jwt.verify(req.cookies.access, secret, (err, token) => {
     const { parentid, id, name } = token;
     if (err) {
